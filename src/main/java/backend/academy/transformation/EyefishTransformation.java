cde23c55cfa11ccf7ce03775f2addb3980cf8bad
Delphington <Delphington@gmail.com>
2024-11-24 23:43:17 +0300

    feat: новые класс фрактального-пламя

--- src/main/java/backend/academy/transformation/EyefishTransformation.java
+++ src/main/java/backend/academy/transformation/EyefishTransformation.java
@@ -3,15 +3,10 @@ package backend.academy.transformation;
 import backend.academy.Point;
 
 public class EyefishTransformation implements Transformation {
-    private double r;
-
-    private double changeCoordinate(double value) {
-        return value * (2 / (r + 1));
-    }
 
     @Override
     public Point apply(Point point) {
-        r = Math.sqrt(Math.pow(point.x(),2) + Math.pow(point.y(), 2));
-        return new Point(changeCoordinate(point.x()), changeCoordinate(point.y()));
+        double r = Math.sqrt(Math.pow(point.x(), 2) + Math.pow(point.y(), 2));
+        return new Point((2/(r+1))*point.x(), (2/(r+1))*point.y());
     }
 }
