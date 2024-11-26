3cd5e38060fbbabd5d5e3bc7579eadb5488d6efe
Delphington <Delphington@gmail.com>
2024-11-26 14:33:28 +0300

    fix: примеры файлов в других форматах

--- src/main/java/backend/academy/transformation/BubbleTransformation.java
+++ src/main/java/backend/academy/transformation/BubbleTransformation.java
@@ -2,10 +2,12 @@ package backend.academy.transformation;
 
 import backend.academy.Point;
 
+@SuppressWarnings("MagicNumber")
 public class BubbleTransformation implements Transformation {
     @Override
     public Point apply(Point point) {
         double r = Math.sqrt(Math.pow(point.x(), 2) + Math.pow(point.y(), 2));
-        return new Point(point.x() * (4 / (Math.pow(r, 2) + 4)), point.y() * (4 / (Math.pow(r, 2) + 4)));
+        double kf = 4 / (Math.pow(r, 2) + 4);
+        return new Point(point.x() * kf, point.y() * kf);
     }
 }
