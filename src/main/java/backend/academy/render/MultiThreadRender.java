1215730be82cecd7f90b06d13d37f781bae63257
Delphington <Delphington@gmail.com>
2024-12-02 21:41:20 +0300

    fix: test

--- src/main/java/backend/academy/render/MultiThreadRender.java
+++ src/main/java/backend/academy/render/MultiThreadRender.java
@@ -30,14 +30,8 @@ public class MultiThreadRender implements Render {
     ) {
 
         for (int num = 0; num < numberSamples; num++) {
-            Runnable task = new Runnable() {
-                @Override
-                public void run() {
-                    transformPoint(fractalImage, new SrvRandomEnvironment(spaces, transformation), symmetry,
-                        iterationSample, rect);
-                }
-            };
-
+            Runnable task = () -> transformPoint(fractalImage, new SrvRandomEnvironment(spaces, transformation), symmetry,
+                iterationSample, rect);
             executorService.execute(task);
         }
         executorService.shutdown();
--- src/test/java/StartProgramTest.java
+++ src/test/java/StartProgramTest.java
@@ -1,131 +1,99 @@
-//import backend.academy.input.InputSimpleParam;
-//import backend.academy.model.FractalImage;
-//import backend.academy.model.Space;
-//import backend.academy.render.MultiThreadRender;
-//import backend.academy.render.Rect;
-//import backend.academy.render.Render;
-//import backend.academy.render.SingleThreadRender;
-//import backend.academy.transformation.Transformation;
-//import java.io.PrintStream;
-//import java.util.List;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//public class StartProgramTest {
-//
-//    private long start;
-//    private long end;
-//    private long resultTimeUser = 2;
-//    private long resultCompareTime = 1;
-//
-//    private final double RECT_X = -2.5;
-//    private final double RECT_Y = -2.5;
-//    private final double RECT_WIDTH = 5.8;
-//    private final double RECT_HEIGHT = 5.2;
-//    private final int DEFAULT_COUNT_THREAD = 10;
-//
-//    private InputSimpleParam srvInput;
-//    private FractalImage fractalImage;
-//    private Transformation transformation;
-//    private List<Space> spaces;
-//    private Render render;
-//    private Render compareRender;
-//
-//    @BeforeEach
-//    public void setInit() {
-//        srvInput = new InputSimpleParam();
-//
-//    }
-//
-//    @Test
-//    @DisplayName("Тест, где пользовать задает единственный поток")
-//    public void startSingleTest() {
-//
-//        srvInput.threadCount(1);
-//
-//        fractalImage = FractalImage.create(srvInput.weight(), srvInput.height());
-//        transformation = srvInput.variousTransformation().get(srvInput.typeTransformation());
-//        spaces = srvInput.spaces();
-//
-//        render = new SingleThreadRender();
-//        compareRender = new MultiThreadRender(DEFAULT_COUNT_THREAD);
-//
-//        start = System.nanoTime();
-//        render.rend(
-//            fractalImage,
-//            spaces,
-//            transformation,
-//            srvInput.sampleCount(),
-//            srvInput.iterationCount(),
-//            srvInput.symmetryCount(),
-//            new Rect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT)
-//        );
-//        end = System.nanoTime();
-//
-//        resultTimeUser = end - start;
-//
-//        start = System.nanoTime();
-//        compareRender.rend(
-//            fractalImage,
-//            spaces,
-//            transformation,
-//            srvInput.sampleCount(),
-//            srvInput.iterationCount(),
-//            srvInput.symmetryCount(),
-//            new Rect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT)
-//        );
-//        end = System.nanoTime();
-//
-//        resultCompareTime = end - start;
-//        System.err.println(resultTimeUser);
-//        System.err.println(resultCompareTime);
-//        assertTrue((resultCompareTime - resultTimeUser) < 0);
-//
-//    }
-//
-//    //
-//    @Test
-//    @DisplayName("Тест, где пользовать множество потоков")
-//    public void startMultiTest() {
-//        srvInput.threadCount(8);
-//        srvInput.printInputConfig();
-//
-//        fractalImage = FractalImage.create(srvInput.weight(), srvInput.height());
-//        transformation = srvInput.variousTransformation().get(srvInput.typeTransformation());
-//        spaces = srvInput.spaces();
-//
-//        render = new MultiThreadRender(srvInput.threadCount());
-//        compareRender = new SingleThreadRender();
-//
-//        start = System.nanoTime();
-//        render.rend(
-//            fractalImage,
-//            spaces,
-//            transformation,
-//            srvInput.sampleCount(),
-//            srvInput.iterationCount(),
-//            srvInput.symmetryCount(),
-//            new Rect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT)
-//        );
-//        end = System.nanoTime();
-//
-//        resultTimeUser = end - start;
-//
-//        start = System.nanoTime();
-//        compareRender.rend(
-//            fractalImage,
-//            spaces,
-//            transformation,
-//            srvInput.sampleCount(),
-//            srvInput.iterationCount(),
-//            srvInput.symmetryCount(),
-//            new Rect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT)
-//        );
-//        end = System.nanoTime();
-//        resultCompareTime = end - start;
-//
-//        assertTrue((resultTimeUser - resultCompareTime) < 0);
-//    }
-//}
+import backend.academy.input.InputSimpleParam;
+import backend.academy.model.FractalImage;
+import backend.academy.model.Space;
+import backend.academy.render.MultiThreadRender;
+import backend.academy.render.Rect;
+import backend.academy.render.Render;
+import backend.academy.render.SingleThreadRender;
+import backend.academy.transformation.Transformation;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Log4j2
+public class StartProgramTest {
+
+    private long start;
+    private long end;
+    private long resultTimeUser;
+    private long resultCompareTime;
+
+    private final double RECT_X = -2.5;
+    private final double RECT_Y = -2.5;
+    private final double RECT_WIDTH = 5.8;
+    private final double RECT_HEIGHT = 5.2;
+    private final int DEFAULT_COUNT_THREAD = 2
+        ;
+
+    private InputSimpleParam srvInput;
+    private FractalImage fractalImage;
+    private Transformation transformation;
+    private List<Space> spaces;
+    private Render render;
+    private Render compareRender;
+
+    @BeforeEach
+    public void setInit() {
+        srvInput = new InputSimpleParam();
+
+    }
+
+    @Test
+    @DisplayName("Тест, где пользовать задает единственный поток")
+    public void startSingleTest() throws InterruptedException {
+        srvInput = new InputSimpleParam();
+
+        srvInput.threadCount(1);
+
+        fractalImage = FractalImage.create(srvInput.weight(), srvInput.height());
+        transformation = srvInput.variousTransformation().get(srvInput.typeTransformation());
+        spaces = srvInput.spaces();
+
+        render = new SingleThreadRender();
+
+        start = System.nanoTime();
+        render.rend(
+            fractalImage,
+            spaces,
+            transformation,
+            srvInput.sampleCount(),
+            srvInput.iterationCount(),
+            srvInput.symmetryCount(),
+            new Rect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT)
+        );
+        Thread.sleep(10000);
+        end = System.nanoTime();
+
+        resultTimeUser = end - start;
+
+        //-------------------------------------------------------------
+        srvInput = new InputSimpleParam();
+
+        srvInput.threadCount(2);
+
+        fractalImage = FractalImage.create(srvInput.weight(), srvInput.height());
+        transformation = srvInput.variousTransformation().get(srvInput.typeTransformation());
+        spaces = srvInput.spaces();
+        compareRender = new MultiThreadRender(DEFAULT_COUNT_THREAD);
+
+        start = System.nanoTime();
+        compareRender.rend(
+            fractalImage,
+            spaces,
+            transformation,
+            srvInput.sampleCount(),
+
+            srvInput.iterationCount(),
+            srvInput.symmetryCount(),
+            new Rect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT)
+        );
+        end = System.nanoTime();
+
+        resultCompareTime = end - start;
+
+        assertTrue((resultCompareTime - resultTimeUser) < 0);
+    }
+}
